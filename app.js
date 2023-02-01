df0bed6af07496fb22155a61948602b25e3f591d
João Gabriel Pereira Modesto <joao.modesto@rede.local>
2023-02-01 15:21:15 -0300

    first-commit

---
+++ app.js
@@ -0,0 +1,29 @@
+function showModal() {
+    var element = document.getElementById("modal")
+    element.classList.add("show-modal");
+  }
+
+  function showModal2() {
+    var element = document.getElementById("modal2")
+    element.classList.add("show-modal");
+  }
+
+  function showModal3() {
+    var element = document.getElementById("modal3")
+    element.classList.add("show-modal");
+  }
+
+  function hideModal() {
+    var element = document.getElementById("modal");
+    element.classList.remove("show-modal");
+  }
+
+  function hideModal2() {
+    var element = document.getElementById("modal2");
+    element.classList.remove("show-modal");
+  }
+
+  function hideModal3() {
+    var element = document.getElementById("modal3");
+    element.classList.remove("show-modal");
+  }
